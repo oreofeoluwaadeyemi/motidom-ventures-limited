744ef71412be6b401bc316281c176bf086ab058e
oreofeoluwaadeyemi <berreladeyemi@gmail.com>
2026-07-03 18:44:03 +0100

    updated cors

--- client/src/api/receiptApi.ts
+++ client/src/api/receiptApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2000/api",
+  baseURL: "https://motidom-ventures-limited-1.onrender.com",
 });
 
 export const createReceipt = (data: any) =>
